9c34aa203759cbcf12e43b5aaad247d498539366
Aries Vincent Dajay <avdajay@gmail.com>
2020-07-20 13:07:48 +0800

    Initial commit

---
+++ song.js
@@ -0,0 +1,32 @@
+var trackTitle = "To The River";
+var artist = "Pentatonix";
+var genre = "Pop";
+var durationInSeconds = 213;
+var yearReleased = "2015"
+var lyricsLanguage = "English";
+var publisher = "Sony Music";
+var isExclusive = true;
+var album = "Pentatonix (Japan Version)";
+
+var myFavoriteMusic = {
+    trackTitle: this.trackTitle,
+    artist: this.artist,
+    genre: this.genre,
+    durationInSeconds: this.durationInSeconds,
+    yearReleased: this.yearReleased,
+    lyricsLanguage: this.lyricsLanguage,
+    publisher: this.publisher,
+    isExlusive: this.isExclusive,
+    album: this.album
+}
+
+console.log(myFavoriteMusic.trackTitle);
+console.log(myFavoriteMusic.artist);
+console.log(myFavoriteMusic.genre);
+console.log(myFavoriteMusic.durationInSeconds);
+console.log(myFavoriteMusic.yearReleased);
+console.log(myFavoriteMusic.lyricsLanguage);
+console.log(myFavoriteMusic.publisher);
+console.log(myFavoriteMusic.isExlusive);
+console.log(myFavoriteMusic.album);
+
